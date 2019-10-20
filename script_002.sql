eee5c576f6062797ef49494fd690b151db6a6224
ehrhardt98 <jorgeavbe@al.insper.edu.br>
2019-10-20 18:49:27 -0300

    desespero

---
+++ script_002.sql
@@ -0,0 +1,34 @@
+USE dbird;
+
+DROP TRIGGER IF EXISTS trig_delete_user;
+delimiter //
+CREATE TRIGGER trig_delete_user
+	AFTER UPDATE ON users
+	FOR EACH ROW
+	BEGIN
+		IF NEW.is_activeu = 0 THEN
+			UPDATE post 
+            INNER JOIN user_bird USING(id_user) 
+            SET is_activep = 0,
+            is_activebm = 0
+			WHERE post.id_user = users.id_user;
+		END IF;
+	END//
+delimiter ;
+
+DROP TRIGGER IF EXISTS trig_delete_post;
+delimiter //
+CREATE TRIGGER trig_delete_post
+	AFTER UPDATE ON post
+	FOR EACH ROW
+	BEGIN
+		IF NEW.is_activep = 0 THEN
+			UPDATE user_mention 
+            INNER JOIN post USING(id_post) 
+            INNER JOIN bird_mention USING(id_post)
+            SET is_activeum = 0,
+            is_activebm = 0
+			WHERE post.id_post = user_mention.id_post AND post.id_post = bird_mention.id_post;
+		END IF;
+	END//
+delimiter ;
---
+++ script_003.sql
@@ -0,0 +1,13 @@
+USE dbird;
+
+CREATE TABLE like_post
+(
+    id_post INT NOT NULL,
+    id_user INT NOT NULL,
+    like_value TINYINT NOT NULL,
+    PRIMARY KEY (id_post, id_user),
+    FOREIGN KEY (id_user) REFERENCES users (id_user),
+    FOREIGN KEY (id_post) REFERENCES post  (id_post)
+);
+
+ALTER TABLE post ADD post_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP;
